e8f6d1d9004f7e36ec7e52edd2deceb55eaa7a0e
oisikurumeronpan <a.takano@quando.jp>
2024-07-05 15:29:41 +0900

    first commit

---
+++ AppNotifyTest/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Windows.AppNotifications;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AppNotifyTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppNotificationManager.Default.NotificationInvoked += (_, __) => { };
+            AppNotificationManager.Default.Register();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            AppNotificationManager.Default.Unregister();
+        }
+    }
+
+}
---
+++ AppNotifyTest/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Windows.AppNotifications.Builder;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Notifications;
+
+namespace AppNotifyTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ToastNotifier notifier = ToastNotificationManager.CreateToastNotifier();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            notifybtn.Click += Notifybtn_Click;
+        }
+
+        private void Notifybtn_Click(object sender, RoutedEventArgs e)
+        {
+            var notification = new AppNotificationBuilder()
+                .AddText("Hello!!")
+                .BuildNotification();
+            var doc = new Windows.Data.Xml.Dom.XmlDocument();
+            doc.LoadXml(notification.Payload);
+            var toast = new ToastNotification(doc);
+            notifier.Show(toast);
+
+            toast.Activated += (_, __) =>
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    new SuccessWindow().Show();
+                });
+            };
+        }
+    }
+}
